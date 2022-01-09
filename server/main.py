43aeb10c737bf0fc0a9d4b1aee50b4cd245a8c0a
Prabu <prabu.muruganandam@gmail.com>
2022-01-09 16:48:26 +0530

    Add db setup

--- server/main.py
+++ server/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from db.utils import connect_to_mongo,close_mongo_connection
+from db.mongodb import db
 
 app = FastAPI()
-origins = [
-    "http://localhost:3001",
-    "http://localhost:3000",
-    "http://frontend:3000"
-]
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,6 +13,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.on_event("startup")
+async def startup_event():
+  await connect_to_mongo()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+  await close_mongo_connection()
+
+
 @app.get("/api")
-def root():
+async def root():
+  database = db.client.stacks
+  document = {'pk': 'nothine','new':'yes'}
+  result = await database.dummy.insert_one(document)
+  print(result)
   return {"content":"Hello from FastAPI!!"}
